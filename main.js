f22cfb5c67d6ce92be28bcec959a2d7f1d40b625
Unfamous Dyson <dougdyson@gmail.com>
2021-12-01 14:49:53 -0800

    add nonce and rename file

---
+++ main.js
@@ -0,0 +1,47 @@
+const { createHmac } = require('crypto');
+
+const calculateBlockHash = (block) => createHmac('sha256', 'secret').update(block).digest('hex');
+
+const createBlock = (data) => {
+  const id        = blockchain.length + 1;
+  const timestamp = Date.now();
+  const prevHash  = blockchain[blockchain.length - 1].hash || 0
+  const nonce     = Math.random();
+  const hash      = calculateBlockHash(id + timestamp + prevHash + data.toString()+nonce);
+  return blockchain.push({id, timestamp, data, hash, prevHash, nonce});
+}
+
+const validateBlockchain = (bc) => {
+  // start comparison from second block
+  for (let i = 1; i < bc.length; i++) {
+    
+    // recalculated hash must match current block's recorded hash
+    const currentBlock = bc[i].id + bc[i].timestamp + bc[i].prevHash + bc[i].data.toString() + bc[i].nonce;
+    if (bc[i].hash != calculateBlockHash(currentBlock)) return false;
+
+    // current block's previous hash must match hash on previous block
+    if (bc[i].prevHash != bc[i - 1].hash) return false;
+  }
+
+  return true;
+}
+
+// create blockchain with genesis block
+const blockchain   = [{id: '1', timestamp: Date.now(), data: {description: 'genesis block'}, hash: '', prevHash: 0}];
+blockchain[0].hash = calculateBlockHash(blockchain[0].toString());
+
+// create some blocks with JSON data
+createBlock({description:'second block'}); 
+createBlock({description:'third block'});
+createBlock({description:'fourth block'});
+
+// validate blockchain integrity
+validateBlockchain(blockchain) //?
+
+// Testing via Quokka in-line IDE evaluation: //?
+blockchain[0].hash     //?
+blockchain[1].prevHash //?
+blockchain[1].hash     //?
+blockchain[2].prevHash //?
+blockchain[2].hash     //?
+blockchain[3].prevHash //?
