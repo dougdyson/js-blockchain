9fa2c0eaa3541d251638ded70461a094f5af0b8e
Unfamous Dyson <dougdyson@gmail.com>
2021-12-02 15:48:25 -0800

    Remove id prop from block

--- blockchain.js
+++ blockchain.js
@@ -8,16 +8,16 @@ const calculateHash = (data) => createHmac('sha256', 'secret').update(data).dige
 // add a new block
 const newBlock = (data) => {
   proofOfWork(difficulty);
-  const id        = blockchain.length + 1;
   const timestamp = Date.now();
   // need to account for genesis block, which does not have a prevHash to reference
   const prevHash  = (blockchain.length === 0) ? null : blockchain[blockchain.length - 1].hash
   const nonce     = Math.floor(Math.random() * timestamp)
-  const hash      = calculateHash(id + timestamp + prevHash + nonce + data);
-  return {id, timestamp, prevHash, nonce, hash, data}
+  const hash      = calculateHash(timestamp + prevHash + nonce + data);
+  return {timestamp, prevHash, nonce, hash, data}
 }
 
 // Proof-of-Work
+// This is not a real PoW, just a fancy delay using hasing for illustrate purposes
 const proofOfWork = (difficulty) => {
   let nonce = 0;
   let hash = calculateHash(nonce.toString())
@@ -41,6 +41,6 @@ blockchain.push(newBlock('fourth block'));
 // =============================================
 const bc2 = blockchain[2];
 // bc2.data  = 'hack';
-const rehash = calculateHash(bc2.id + bc2.timestamp + bc2.prevHash + bc2.nonce + bc2.data);
+const rehash = calculateHash(bc2.timestamp + bc2.prevHash + bc2.nonce + bc2.data);
 // false if hacked, true if not
 (bc2.hash === rehash) ? true : false; //?
