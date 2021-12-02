438dd6e7d86542bbb5a0d003c54f76f6977c9210
Unfamous Dyson <dougdyson@gmail.com>
2021-12-01 16:39:44 -0800

    debugging

--- main.js
+++ main.js
@@ -1,42 +1,44 @@
 const { createHmac } = require('crypto');
 
-const calculateBlockHash = (block) => createHmac('sha256', 'secret').update(block).digest('hex');
+const blockchain = [];
 
-const createBlock = (data) => {
+const calculateHash = (data) => createHmac('sha256', 'secret').update(data).digest('hex');
+
+const createBlock = (data) => {  
   const id        = blockchain.length + 1;
   const timestamp = Date.now();
-  const prevHash  = blockchain[blockchain.length - 1].hash || 0
-  const nonce     = Math.random();
-  const hash      = calculateBlockHash(id + timestamp + prevHash + data.toString() + nonce);
-  return blockchain.push({id, timestamp, data, hash, prevHash, nonce});
+  // genesis block doesn't have access to a prevHash so need to provide a value
+  const prevHash  = (blockchain.length === 0) ? null : blockchain[blockchain.length - 1].hash
+  const nonce     = Math.floor(Math.random() * timestamp);
+  const hash      = calculateHash(id + timestamp + prevHash + nonce + data.toString());
+  return blockchain.push({id, timestamp, hash, prevHash, nonce, data});
 }
 
-const validateBlockchain = (bc) => {
+const validateBlockchain = (blockchain) => {
   // start comparison from second block
-  for (let i = 1; i < bc.length; i++) {
+  for (let i = 1; i < blockchain.length; i++) {
+    const block = blockchain[i];
     
     // recalculated hash must match current block's recorded hash
-    const currentBlock = bc[i].id + bc[i].timestamp + bc[i].prevHash + bc[i].data.toString() + bc[i].nonce;
-    if (bc[i].hash != calculateBlockHash(currentBlock)) return false;
+    const currentBlock = block.id + block.timestamp + block.prevHash + block.nonce + block.data.toString();
+    if (block.hash != calculateHash(currentBlock)) return false;
 
     // current block's previous hash must match hash on previous block
-    if (bc[i].prevHash != bc[i - 1].hash) return false;
+    if (block.prevHash != blockchain[i - 1].hash) return false;
   }
-
   return true;
 }
 
-// create blockchain with genesis block
-const blockchain   = [{id: '1', timestamp: Date.now(), data: {description: 'genesis block'}, hash: '', prevHash: 0}];
-blockchain[0].hash = calculateBlockHash(blockchain[0].toString());
 
+// ===============================================
 // create some blocks with JSON data
+createBlock({description:'genesis block'})
 createBlock({description:'second block'}); 
 createBlock({description:'third block'});
 createBlock({description:'fourth block'});
 
+//================================================
 // Testing via Quokka in-line IDE evaluation: //?
-// validate blockchain integrity
 validateBlockchain(blockchain) //?
 blockchain[0].hash     //?
 blockchain[1].prevHash //?
@@ -44,3 +46,9 @@ blockchain[1].hash     //?
 blockchain[2].prevHash //?
 blockchain[2].hash     //?
 blockchain[3].prevHash //?
+
+// hack data on the blockchain
+blockchain[2].data.description; //?
+blockchain[2].data.description = 'hack'
+blockchain[2] //?
+validateBlockchain(blockchain) //?
