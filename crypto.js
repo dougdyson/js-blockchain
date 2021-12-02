51592313fc4ce32a9e28a7d4c24b5a9b7101dfd9
Unfamous Dyson <dougdyson@gmail.com>
2021-12-01 16:40:27 -0800

    cleanup for readability

--- crypto.js
+++ crypto.js
@@ -7,7 +7,7 @@ const array = [];
 
 const calculateHash = (data) => createHmac('sha256', 'secret').update(data).digest('hex');
 
-// add a block!
+// add a block
 array.push({hash: calculateHash('a').toString(), data: 'a'});
 
 // here's the hash
@@ -16,5 +16,6 @@ array[0]; //?
 // hack the block data!
 array[0].data = 'b';
 calculateHash('b'); //?
+
 // hash won't match for hacked data
 (array[0].hash != calculateHash(array[0].data)) //?
