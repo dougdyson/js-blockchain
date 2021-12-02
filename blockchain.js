eae460dd6bddf98ef8b9d37c368a564c9625b2de
Unfamous Dyson <dougdyson@gmail.com>
2021-12-01 21:06:15 -0800

    clean code

--- blockchain.js
+++ blockchain.js
@@ -8,9 +8,10 @@ const calculateHash = (data) => createHmac('sha256', 'secret').update(data).dige
 const newBlock = (data) => {
   const id        = blockchain.length - 1;
   const timestamp = Date.now();
+  // need to account for genesis block, which does not have a prevHash to reference
   const prevHash  = (blockchain.length === 0) ? null : blockchain[blockchain.length - 1].hash
-  const nonce     = Math.floor(Math.random() * id)
-  const hash      = calculateHash(id + timestamp + prevHash + data);
+  const nonce     = Math.floor(Math.random() * timestamp)
+  const hash      = calculateHash(id + timestamp + prevHash + nonce + data);
   return {id, timestamp, prevHash, nonce, hash, data}
 }
 
@@ -27,13 +28,8 @@ blockchain.push(newBlock('fourth block'));
 // TESTING
 // =============================================
 const bc = blockchain[1];
+bc.data  = 'hack';
 
-bc.data; //?
-bc.hash //?
-
-bc.data = 'hack';
-bc.data; //?
-const hashableFields = bc.id + bc.timestamp + bc.prevHash + bc.data;
-const hack = calculateHash(hashableFields); //?
+const hack = calculateHash(bc.id + bc.timestamp + bc.prevHash + bc.nonce + bc.data); //?
 
 (bc.hash === hack) ? true : false //?
