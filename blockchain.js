ffe517cfd42775a8725032aa2c1c23f9651c9074
Unfamous Dyson <dougdyson@gmail.com>
2021-12-03 10:47:15 -0800

    prettier

--- blockchain.js
+++ blockchain.js
@@ -1,18 +1,26 @@
 const { createHmac } = require('crypto');
-const { get } = require('https');
 
 const calculateHash = (data) => createHmac('sha256', 'secret').update(data).digest('hex');
+
+// Proof-of-Work (PoW)
+// This is not a real PoW, just a loop using hashing for illustrative purposes
+// Was first rehashing block props while incrementing its nonce but it was TOO SLOW!!
+// (p.s. PoW is essentially a transaction throttle)
+const proofOfWork = () => {
+  const difficulty = 1
+  let nonce = 0;
+  let hash  = '';
+  while (hash.substring(0, difficulty) != Array(difficulty + 1).join('0')) {
+    nonce++;
+    hash = calculateHash(nonce.toString());
+  }
+}
 
 class Blockchain {
   
   constructor() {
     this.chain = [];
     this.addGenesisBlock();
-  }
-
-  getLastBlock(){
-    console.log(this.chain[this.chain.length - 1]);
-    return ;
   }
 
   addGenesisBlock(){
@@ -33,20 +41,6 @@ class Blockchain {
     return this.chain.push(newBlock);
   }
 
-}
-
-// Proof-of-Work (PoW)
-// This is not a real PoW, just a loop using hashing for illustrative purposes
-// Was first rehashing block props while incrementing its nonce but it was TOO SLOW!!
-// (p.s. PoW is essentially a transaction throttle)
-const proofOfWork = () => {
-  const difficulty = 1
-  let nonce = 0;
-  let hash  = '';
-  while (hash.substring(0, difficulty) != Array(difficulty + 1).join('0')) {
-    nonce++;
-    hash = calculateHash(nonce.toString());
-  }
 }
 
 module.exports = { Blockchain };
