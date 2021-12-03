3927ba62d4fa27cea731633283e9ba915245a03d
Unfamous Dyson <dougdyson@gmail.com>
2021-12-03 09:29:37 -0800

    refactor as classes

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
-const { newBlockchain, newBlock } = require('./blockchain');
+const { Blockchain, Block } = require('./blockchain');
 
 const chain = newBlockchain(1);
 
-const addBlock = (transaction) => {
-  const lastHash = chain[chain.length - 1].hash;
-  chain.push(newBlock(transaction, lastHash));
-};
 
-addBlock('second block');
 
 for (const block of chain) {
   console.log(block);
