7067bc74da415d8ff2905d282adbbe689ffa6bad
Unfamous Dyson <dougdyson@gmail.com>
2021-12-03 23:56:16 -0800

    add getAddressBalance method

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const simplechain = new Blockchain;
 const tx1 = new Transaction('Bob', 'Alice', 100);
 const tx2 = new Transaction('Alice', 'Bob', 50);
 
-simplechain.addBlock(tx1);
-simplechain.addBlock(tx2);
+simplechain.addTransaction(tx1);
+simplechain.addTransaction(tx2);
 
 console.log(simplechain);
