ddc86bae459802279448ae977c46272e0b8c97c1
Unfamous Dyson <dougdyson@gmail.com>
2021-12-03 18:01:31 -0800

    test stuff

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ const { Blockchain } = require('./blockchain');
 
 const simplechain = new Blockchain;
 
-simplechain.setPoWDifficulty(6);
+simplechain.setPoWDifficulty(0);
 
 simplechain.addBlock('second block');
+simplechain.addBlock('third block');
 
 console.log(simplechain);
 
+const lastBlock = simplechain.getLastBlock();
+console.log(`lastBlock: ` + lastBlock.transaction);
