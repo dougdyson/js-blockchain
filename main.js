c459de15ac84189b648332ecd50fb4e5035075a9
Unfamous Dyson <dougdyson@gmail.com>
2021-12-04 00:07:21 -0800

    test getAddressBalance

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const { Transaction } = require('./transaction');
 const simplechain = new Blockchain;
 
 const tx1 = new Transaction('Bob', 'Alice', 100);
-const tx2 = new Transaction('Alice', 'Bob', 50);
+const tx2 = new Transaction('Alice', 'Bob', 150);
 
 simplechain.addTransaction(tx1);
 simplechain.addTransaction(tx2);
