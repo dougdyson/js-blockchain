7dd913928a3fe133e5f55fcbcd0faf1d4951cd25
Unfamous Dyson <dougdyson@gmail.com>
2021-12-07 03:13:03 -0800

    add pending transactions and mining

--- main.js
+++ main.js
@@ -1,13 +1,4 @@
 const { Blockchain } = require('./blockchain');
 
 
-const simplechain = new Blockchain;
-
-(simplechain.addTransaction('Bob', 'genesisblock', 100)) ? console.log(simplechain) : console.log('Nay!');
-
-// simplechain.addTransaction('Alice', 'Bob', 50);
-
-// console.log(simplechain);
-
-console.log(`genesis block balance: ${simplechain.getAddressBalance('genesis block')}`);
-// console.log(`Alice's balance: ${simplechain.getAddressBalance('Alice')}`);
+const simplechain = new Blockchain; //?
