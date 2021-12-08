187e386f572c3201b53a3a26a4e5845e592eda19
Unfamous Dyson <dougdyson@gmail.com>
2021-12-08 04:15:42 -0800

    construct Wallet public key from private key

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-const { Wallet} = require('./wallet');
+const { Wallet } = require('./wallet');
 const { Blockchain } = require('./blockchain');
 
-const wallet = new Wallet()
+const wallet = new Wallet('e64c724907b31a43496df63d8634049c71129de932a80617dbd0672ed5e8a16b');
 
-const simplechain = new Blockchain;
+
+
+
 
