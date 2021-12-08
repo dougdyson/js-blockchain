3f726f95b4352229d583b4fefe6bd2defc3ed99e
Unfamous Dyson <dougdyson@gmail.com>
2021-12-08 05:52:55 -0800

    cleaning up

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const fromWallet = new Wallet('4d545846f94d031cf5682607f72f98106b29735802a350376
 const toWallet = new Wallet('e64c724907b31a43496df63d8634049c71129de932a80617dbd0672ed5e8a16b');
 
 const toAddress = toWallet.publicKey;
-const fromAddress = fromWallet.publicKey;
 
 const blockchain = new Blockchain; //?
 
