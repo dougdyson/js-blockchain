647eba4c88612a22c0ef7035bf538c7a377e65fa
Unfamous Dyson <dougdyson@gmail.com>
2021-12-08 03:31:50 -0800

    add .gitignore!

--- blockchain.test.js
+++
@@ -1,8 +0,0 @@
-import { Blockchain } from "./blockchain";
-
-test('creates a genesis block', () => {
-  const testChain = new Blockchain;
-  const testBlock = {timestamp: Date.now(), hash: '', prevHash: '', toAddress: 'genesis-block', fromAddress: '', amount: 0}
-  testBlock.hash = testChain.calculateHash(timestamp + prevHash + toAddress + fromAddress + amount);
-  expect(testChain.addGenesisBlock).toEqual(testBlock)
-});
