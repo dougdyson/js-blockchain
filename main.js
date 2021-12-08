d27ceaa0aaed5b498ccc20a96cd57312dad1f516
Unfamous Dyson <dougdyson@gmail.com>
2021-12-08 10:31:28 -0800

    fiddle some tests

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 const { Wallet } = require('./wallet');
 const { Blockchain } = require('./blockchain');
 
-const fromWallet = new Wallet('4d545846f94d031cf5682607f72f98106b29735802a350376a0c84f35bdb1a42');
-const toWallet   = new Wallet('e64c724907b31a43496df63d8634049c71129de932a80617dbd0672ed5e8a16b');
+const minerWallet = new Wallet('4d545846f94d031cf5682607f72f98106b29735802a350376a0c84f35bdb1a42');
+const someWallet  = new Wallet('e64c724907b31a43496df63d8634049c71129de932a80617dbd0672ed5e8a16b');
 
 const blockchain = new Blockchain;
 
-const transaction = fromWallet.newTransaction(toWallet.publicKey, 50);
+blockchain.minePendingTransactions(minerWallet.publicKey);
+
+const transaction = minerWallet.newTransaction(someWallet.publicKey, 50);
 
 blockchain.addPendingTransaction(transaction);
 
-blockchain.minePendingTransactions(toWallet.publicKey);
+blockchain.minePendingTransactions(minerWallet.publicKey);
+blockchain.minePendingTransactions(minerWallet.publicKey);
 
-blockchain.minePendingTransactions(toWallet.publicKey);
-
-blockchain.getAddressBalance(toWallet.publicKey); //?
+blockchain.getAddressBalance(minerWallet.publicKey); //?
+blockchain.getAddressBalance(someWallet.publicKey); //?
 
 blockchain;//?
