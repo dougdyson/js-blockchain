c4a5a513c146d896ae464c8e59ae09c4b3304784
Unfamous Dyson <dougdyson@gmail.com>
2021-12-08 06:41:17 -0800

    improve readability

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ const { Blockchain } = require('./blockchain');
 const fromWallet = new Wallet('4d545846f94d031cf5682607f72f98106b29735802a350376a0c84f35bdb1a42');
 const toWallet   = new Wallet('e64c724907b31a43496df63d8634049c71129de932a80617dbd0672ed5e8a16b');
 
-const transaction = fromWallet.newTransaction(toWallet.publicKey, 50);
-
 const blockchain = new Blockchain;
+
+const transaction = fromWallet.newTransaction(toWallet.publicKey, 50);
 
 blockchain.addPendingTransaction(transaction);
 
