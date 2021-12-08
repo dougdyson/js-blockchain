24645c904fc8e0953180bba3b4fe0871712425a6
Unfamous Dyson <dougdyson@gmail.com>
2021-12-08 06:07:44 -0800

    lint

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const { Wallet } = require('./wallet');
 const { Blockchain } = require('./blockchain');
 
 const fromWallet = new Wallet('4d545846f94d031cf5682607f72f98106b29735802a350376a0c84f35bdb1a42')
-const toWallet = new Wallet('e64c724907b31a43496df63d8634049c71129de932a80617dbd0672ed5e8a16b');
+const toWallet   = new Wallet('e64c724907b31a43496df63d8634049c71129de932a80617dbd0672ed5e8a16b');
 
 const toAddress = toWallet.publicKey;
 
