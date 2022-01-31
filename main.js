ab8586edd6512d585c4f0a02fabc17f0cc0b2081
Unfamous Dyson <HatHeadNinja@users.noreply.github.com>
2022-01-30 23:20:33 -0800

    add comment

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const { Blockchain } = require('./blockchain');
 const minerWallet = new Wallet('4d545846f94d031cf5682607f72f98106b29735802a350376a0c84f35bdb1a42');
 const toWallet    = new Wallet('e64c724907b31a43496df63d8634049c71129de932a80617dbd0672ed5e8a16b');
 
-// pass token symbol, initial pow difficulty and mining reward to create new blockchain
+// pass in token symbol, initial pow difficulty and mining reward to create new blockchain
 const blockchain = new Blockchain('SMPL', 1, 1);
 
 // mine and reward
